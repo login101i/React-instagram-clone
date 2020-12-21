2c47d251023e33b047d4d62ea4a670a3a52a106e
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2020-12-21 19:35:52 +0100

    menu update

--- src/App.js
+++ src/App.js
@@ -8,145 +8,322 @@ import Post from './components/Post'
 import { auth, db } from './firebase'
 import ImageUpload from './components/ImageUpload';
 import InstagramEmbed from 'react-instagram-embed';
-import LoginRegister from './components/LoginRegister'
+import MenuPopupState from './components/MenuPopupState'
+import getUserLocale from 'get-user-locale'
 
 
 
+import { makeStyles } from '@material-ui/core/styles';
+
+
+
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}`,
+    left: `${left}`,
+    transform: `translate(-${top}%,-${top}%)`
+  }
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    top: "50%",
+    left: '50%',
+    width: 600,
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    borderRadius: 10,
+  },
+
+})
+);
+
+const Spinner = () => {
+  <div className="post__">
+    <img src="https://i.gifer.com/ZZ5H.gif" alt="Loading" width="20" />
+    <h5>Loading</h5>
+  </div>
+}
+
+const locale = () => {
+  if (getUserLocale().includes('pl')) {
+    return (true)
+  } else {
+    return (false)
+  }
+
+}
 
 
 
 
 function App() {
   const [posts, setPosts] = useState([])
-  const [openSignUp, setOpenSignUp] = useState(false)
   const [logged, setLogged] = useState(false)
-  const [openSignIn, setOpenSignIn] = useState('')
-
+  const [openSignIn, setOpenSignIn] = useState(false)
+  const [openSignUp, setOpenSignUp] = useState(false)
   const [username, setUserName] = useState("")
   const [user, setUser] = useState(null)
+  const classes = useStyles()
+  const [modalStyle, setModalStyle] = useState(getModalStyle)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [lang, setLang] = useState(locale);
 
 
-  useEffect(() => {
-    db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc => ({
-        id: doc.id,
-        post: doc.data()
-      })))
-    })
-  }, [])
 
-  // ____________________________________________________________________
 
-  useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        setUser(authUser)
-        console.log(authUser)
+  const toggleLang = () => setLang(!lang);
 
-        if (authUser.displayName) {
-          // nie uaktualniaj imienia
-        } else {
-          // jeśli stworzyliśmy nowego
-          return authUser.updateProfile({
-            displayName: username
-          })
-        }
+
+
+
+
+useEffect(() => {
+  db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+    setPosts(snapshot.docs.map(doc => ({
+      id: doc.id,
+      post: doc.data()
+    })))
+  })
+}, [])
+
+// ____________________________________________________________________
+
+useEffect(() => {
+  auth.onAuthStateChanged((authUser) => {
+    if (authUser) {
+      setUser(authUser)
+      console.log(authUser)
+
+      if (authUser.displayName) {
+        // nie uaktualniaj imienia
       } else {
-        setUser(null)
+        // jeśli stworzyliśmy nowego
+        return authUser.updateProfile({
+          displayName: username
+        })
       }
+    } else {
+      setUser(null)
+    }
+  })
+}, [user, username])
+
+
+
+
+const signUp = (event) => {
+  event.preventDefault()
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      authUser.user.updateProfile({
+        displayName: username
+      })
+      setOpenSignUp(false)
     })
-  }, [user, username])
+    .catch((error) => alert(error.message))
+}
 
 
+const signIn = (event) => {
+  event.preventDefault()
+  setOpenSignIn(true)
+  console.log("hhihihi")
 
-  // ____________________________________________________________________
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => alert(error.message))
+
+  setOpenSignIn(false)
+
+}
 
 
-  // Rejestracja
-  return (
-    <div className="app">
-
-      <div className="app__header">
+// ____________________________________________________________________
 
 
-        <img
-          className="app__headerImage"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png" alt="" />
+// Rejestracja
+return (
+  <div className="app">
 
-        <div className="app__logIn">
-          {!user ? (<Button onClick={() => setOpenSignIn(true)}>Zaloguj się</Button>)
-            :
-            (<Button onClick={() => auth.signOut()}>Wyloguj się</Button>)}
+    <div className="app__header">
 
-          <Button Button onClick={() => setOpenSignUp(true)}>Zarejestruj się</Button>
+
+      <img
+        className="app__headerImage"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png" alt="" />
+
+      <div className="app__logIn">
+        {!user ? (<Button onClick={() => setOpenSignIn(true)}>Zaloguj się</Button>)
+          :
+          (<Button onClick={() => auth.signOut()}>Wyloguj się</Button>)}
+
+        <Button Button onClick={() => setOpenSignUp(true)}>Zarejestruj się</Button>
+        <MenuPopupState
+          topmenu={true}
+          lang={lang}
+          user={user}
+          functiontopass={toggleLang}
+          labeltopass={lang ? "English Version" : "Wersja Polska"}
+          signout={() => auth.signOut()}
+          signoutlabel={lang ? "Wyloguj" : "Logout"}
+          signin={() => setOpenSignIn(true)}
+          signinlabel={lang ? "Zaloguj się" : "Sign In"}
+          signup={() => setOpenSignUp(true)}
+          signuplabel={lang ? "Zarejestruj się" : "Sign Up"}
+
+        />
+      </div>
+    </div>
+
+    {/* Rejestracja */}
+
+    <Modal
+      className={modalStyle}
+      open={openSignUp}
+      onClose={() => setOpenSignUp(false)}
+
+    >
+      <div style={modalStyle} className={classes.paper}>
+        <center>
+          <img
+            className="app__headerImage"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png" alt="" />
+        </center>
+        <div className="app__inputs">
+          <Input
+            placeholder="username"
+            type="text"
+            value={username}
+            onChange={e => setUserName(e.target.value)}
+          />
+          <Input
+            placeholder="email/login"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="hasło"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <Button onClick={signUp}>Zarejestruj się</Button>
+
+
         </div>
+
+
       </div>
 
-      {/* Rejestracja */}
 
-   
-      {/* Logowanie */}
+    </Modal>
 
-      <LoginRegister
-      
-      />
+    <Modal
+      className={modalStyle}
+      open={openSignIn}
+      onClose={() => setOpenSignIn(false)}
+    >
+      <div style={modalStyle} className={classes.paper}>
+        <center>
+          <img
+            className="app__headerImage"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png" alt="" />
+        </center>
+        <div className="app__inputs">
 
-
-      <div className="app_posts">
-
-        <div className="app__postsLeft">
-          {
-            posts.map(({ id, post }) => (
-              <Post
-                key={post.id}
-                postId={id}
-                username={post.username}
-                caption={post.caption}
-                imageUrl={post.imageUrl}
-
-              />
-            ))
-          }
-        </div>
-
-
-        <div className="app__postsRight">
-
-          <InstagramEmbed
-            url="https://instagr.am/p/CAX8psZMEdL_Lkto_rA_8oIhfVE1IJNLUobpkc0/"
-            maxWidth={320}
-            hideCaption={false}
-            containerTagName="div"
-            protocol=""
-            injectScript
-            onLoading={() => { }}
-            onSuccess={() => { }}
-            onAfterRender={() => { }}
-            onFailure={() => { }}
+          <Input
+            placeholder="email/login"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="hasło"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
 
+          <Button onClick={signIn}>Zaloguj się</Button>
+
         </div>
+
+
       </div>
 
 
-      <Post
-        username="Maciej Kruszyniak"
-        caption="Caption 1"
-        imageUrl="https://www.jetsetter.com/wp-content/uploads/sites/7/2018/04/VUC2Bht0-1380x690.jpeg" />
+    </Modal>
+    {/* Logowanie */}
 
 
 
-      {user?.displayName ? (
-        <ImageUpload
-          username={user.displayName}
+    <div className="app_posts">
+
+      <div className="app__postsLeft">
+        {
+          posts.map(({ id, post }) => (
+            <Post
+              user={user}
+              key={post.id}
+              postId={id}
+              username={post.username}
+              caption={post.caption}
+              imageUrl={post.imageUrl}
+
+            />
+          ))
+        }
+      </div>
+
+
+      <div className="app__postsRight">
+
+        <InstagramEmbed
+          url="https://instagr.am/p/CAX8psZMEdL_Lkto_rA_8oIhfVE1IJNLUobpkc0/"
+          maxWidth={320}
+          hideCaption={false}
+          containerTagName="div"
+          protocol=""
+          injectScript
+          onLoading={() => { }}
+          onSuccess={() => { }}
+          onAfterRender={() => { }}
+          onFailure={() => { }}
         />
-      ) : (<h3>Przepraszam, musisz się najpierw zalogować</h3>)}
+
+      </div>
+    </div>
+
+
+    <Post
+      username="Maciej Kruszyniak"
+      caption="Caption 1"
+      imageUrl="https://www.jetsetter.com/wp-content/uploads/sites/7/2018/04/VUC2Bht0-1380x690.jpeg" />
+
+
+
+    {user?.displayName ? (
+      <ImageUpload
+        username={user.displayName}
+      />
+    ) : (<h3>Przepraszam, musisz się najpierw zalogować</h3>)}
 
 
 
 
-    </div >
-  );
+  </div >
+);
 }
 
 export default App;
--- src/components/Post.js
+++ src/components/Post.js
@@ -4,10 +4,11 @@ import React, { useState, useEffect } from 'react'
 
 import { db } from '../firebase'
 import './post.css'
+import firebase from 'firebase'
 
-function Post({ username, caption, imageUrl, postId }) {
-    const [comments, setComments] = useState('')
-    const [comment, setComment] = useState('')
+function Post({ username, caption, imageUrl, postId, user }) {
+    const [comments, setComments] = useState([])
+    const [comment, setComment] = useState([])
 
     useEffect(() => {
         let unsubscribe;
@@ -15,20 +16,29 @@ function Post({ username, caption, imageUrl, postId }) {
             unsubscribe = db
                 .collection("posts")
                 .doc(postId)
-                .collection("comments")
+                .collection("123")
                 .orderBy('timestamp', 'asc')
                 .onSnapshot((snapshot) => {
                     setComments(snapshot.docs.map((doc) => doc.data()));
                 })
         }
         return () => {
-            unsubscribe();
+            // unsubscribe();
+            console.log(comments)
+
         };
     }, [postId]);
+    console.log(comments)
 
 
     const postComment = (event) => {
         event.preventDefault()
+        db.collection("posts").doc(postId).collection('123').add({
+            comment: comment,
+            username: user.displayName,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        setComment('')
     }
 
 
@@ -48,27 +58,42 @@ function Post({ username, caption, imageUrl, postId }) {
                 src={imageUrl} alt="" />
             <h4 className="post__text">{caption} <strong>{username}</strong></h4>
 
-            <div className="post__commentBox">
-                <form>
-                    <input
-                        className="post__input"
-                        type="text"
-                        placeholder="Twój komentarz"
-                        value={comment}
-                        onChange={(e) => setComments(e.target.value)}
-                    />
-                </form>
-                <button
-                    classNme="post__button"
-                    disabled={!comment}
-                    type="submit"
-                    onClick="postComment"
-                >
-                    Opublikuj
-            </button>
 
+            <div className="post__comments">
+                {comments.map((comment) => (
+                    <p>
+                        <strong>{comment.username}</strong>{comment.comment}
+                    </p>
+                ))}
             </div>
-        </div>
+            {user &&
+                <form>
+                    <div className="post__commentBox">
+
+                        <input
+                            className="post__input"
+                            type="text"
+                            placeholder="Twój komentarz"
+                            value={comment}
+                            onChange={(e) => setComment(e.target.value)}
+                        />
+
+                        <button
+                            className="post__button"
+                            disabled={!comment}
+                            type="submit"
+                            onClick={postComment}
+                        >
+                            Opublikuj
+                  </button>
+
+
+                    </div>
+                </form>
+            }
+
+
+        </div >
 
 
     )
